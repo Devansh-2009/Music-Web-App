1d48facc30a9fe41e708e46e15975cd85aa5f0d4
Devansh-2009 <87084391+Devansh-2009@users.noreply.github.com>
2022-05-20 16:40:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -8,10 +12,29 @@ function setup(){
 
     video= createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+}
+function modelLoaded(){
+    console.log('Pose Net is initialized');
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
 function draw(){
     Image(video, 0, 0, 600, 500); 
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
